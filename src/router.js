2b6998b9cc22c0d25fccd26da87d752c9800ffb8
AtoraYamada <atora2018@gmail.com>
2019-08-26 10:07:01 +0900

    set Toppage frame

--- src/router.js
+++ src/router.js
@@ -1,8 +1,21 @@
 import Vue from "vue";
 import Router from "vue-router";
 
+import Footer from "./components/Footer.vue"
+
+import TopHeader from "./components/Top/TopHeader.vue";
+import TopLink from "./components/Top/TopLink.vue";
+
 Vue.use(Router);
 
 export default new Router({
   mode: 'history',
+  routes: [
+    { path: '/',
+      components: {
+      header: TopHeader,
+      link: TopLink,
+      default: Footer
+    }}
+  ]
 })
