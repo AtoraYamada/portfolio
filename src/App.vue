8eec978f9b17483d57e4c87cd6c66af8a64c644f
AtoraYamada <atora2018@gmail.com>
2019-08-26 23:21:21 +0900

    merge Footer with App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,47 @@
 <template>
   <div>
-    <router-view name="header"></router-view>
-    <router-view name="link"></router-view>
-    <router-view></router-view>
+    <main>
+      <router-view name="header"></router-view>
+      <router-view name="link"></router-view>
+    </main>
+    <footer>
+      <p>
+        日本茶と<br>
+        プログラミングは<br>
+        違えども<br>
+        奥深きこと<br>
+        おなじ所なり
+      </p>
+    </footer>
   </div>
 </template>
 
-<script>
-
-</script>
+<style scoped>
+  main {
+    position: relative;
+    z-index: 2;
+    margin-bottom: 400px;
+    -webkit-box-shadow: #000 0 7px 35px 0;
+    -moz-box-shadow: #000 0 7px 35px 0;
+    box-shadow: #000 0 7px 35px 0;
+  }
+  footer {
+    text-align: left;
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    height: 400px;  
+    background: #2A2A2A;
+    font-size: calc(2.2rem + ((1vw - 0.64rem) * 2.1429));
+    color: white; 
+    line-height: calc(5.0rem + ((1vw - 0.64rem) * 2.1429));
+  }
+  footer p{
+    margin: auto;
+    padding-top: 2%;
+    font-family: 'PMingLiU';
+    writing-mode: vertical-rl;
+    -ms-writing-mode: tb-rl;
+    -webkit-writing-mode: vertical-rl;
+  }
+</style>
