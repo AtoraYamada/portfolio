3fda5adb50f5ffe62d4ff0c458ae00f4a25e9062
AtoraYamada <atora2018@gmail.com>
2019-08-27 13:15:48 +0900

    adjust top footer

--- src/components/Top/Footer.vue
+++ src/components/Top/Footer.vue
@@ -18,12 +18,12 @@
     width: 100%;
     height: 400px;  
     background: #2A2A2A;
-    font-size: calc(2.2rem + ((1vw - 0.64rem) * 2.1429));
+    font-size: calc(1.4rem + ((1vw - 0.64rem) * 2.1429));
     color: white; 
     line-height: calc(5.0rem + ((1vw - 0.64rem) * 2.1429));
   }
   footer p{
-    margin: auto;
+    margin: 50px auto;
     padding-top: 3%;
     font-family: 'PMingLiU';
     writing-mode: vertical-rl;
