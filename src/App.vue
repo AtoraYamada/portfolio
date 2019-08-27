2f4462e760224888900fa1d1a2eee48bc32d0b97
AtoraYamada <atora2018@gmail.com>
2019-08-27 15:43:32 +0900

    adjust page layout

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,55 @@
 <template>
   <div>
-    <router-view></router-view>
-    <router-view name="footer"></router-view>>
+    <main>
+      <TopHeader></TopHeader>
+      <TopLink></TopLink>
+    </main>
+    <router-view name="content"></router-view>
+    <p><router-link to="/">TOP PAGE</router-link></p>
   </div>
 </template>
+
+<script>
+import TopHeader from "./components/Top/TopHeader.vue";
+import TopLink from "./components/Top/TopLink.vue";
+
+export default {
+  components: {
+    TopHeader,
+    TopLink
+  }
+}
+</script>
+
+<style scoped>
+  main {
+    position: relative;
+    z-index: 2;
+    margin-bottom: 350px;
+    /* -webkit-box-shadow: #000 0 7px 35px 0;
+    -moz-box-shadow: #000 0 7px 35px 0;
+    box-shadow: #000 0 7px 35px 0; */
+  }
+  p {
+    position: fixed;
+    bottom: 20px;
+    right: 20px;
+    font-size: 77%;
+    z-index: 3;
+    font-weight: bold;
+  }
+  p a {
+    background: rgb(219, 7, 7);
+    text-decoration: none;
+    color: white;
+    width: 80px;
+    padding: 30px 0;
+    text-align: center;
+    display: block;
+    border-radius: 5px;
+  }
+  p a:hover {
+    text-decoration: none;
+    background: rgb(219, 53, 53);
+  }
+</style>
--- src/router.js
+++ src/router.js
@@ -1,11 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 
-import Top from "./components/Top/Top.vue";
-import TopHeader from "./components/Top/TopHeader.vue";
-import TopLink from "./components/Top/TopLink.vue";
 import Footer from "./components/Top/Footer.vue";
-
 import Contact from "./components/Contact/Contact.vue";
 
 Vue.use(Router);
@@ -15,13 +11,12 @@ export default new Router({
   routes: [
     { path: '/',
       components: {
-        default: Top,
-        footer: Footer
+        content: Footer
       }
     }, {
       path: '/contact',
       components: {
-        default: Contact
+        content: Contact
       }
     }
   ]
