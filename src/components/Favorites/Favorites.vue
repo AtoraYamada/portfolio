135f7fa182089001987f381932bd0844b151f41a
AtoraYamada <atora2018@gmail.com>
2019-08-29 23:51:46 +0900

    create Favorites.vue

---
+++ src/components/Favorites/Favorites.vue
@@ -0,0 +1,148 @@
+<template>
+  <div class="content">
+    <div class="sencha">
+      <div class="sencha--content">
+        <h1>煎茶道</h1>
+        <p>
+          　愛知県に家元をおく流派『薫風流煎茶道』の師範職をもっております。<br>
+          　『煎茶道』とは茶道の一種です。茶道と聞くと大抵の方は抹茶を思い浮かべるかと思いますが、煎茶道は急須でお茶を淹れる茶道で江戸時代からの歴史があります。茶道ですのできちんとした作法・流派が存在しており、全日本煎茶道連盟に加入している流派は2019年時点で34流派となっております。<br>
+          　煎茶道との出会いは東京大学のサークルでした。その名も『東京大学薫風流煎茶同好会』。毎週、東大内の和室に煎茶道の先生が来てくださり、お稽古をつけてくれました。大学1年生（2012年）から大学を卒業した現在まで続けております。<br>
+          　なぜ、煎茶道サークルに入ろうと思ったのか。大学で煎茶道に出会うまでその名前すら聞いたことがありませんでしが、100枚以上のサークルビラをめくっていた時に偶然見つけ「茶道だけど抹茶じゃない！？おもしろそう！」と直感したのが始まりです。実際に体感してみて初日で入部を即決しました。<br>
+          　急須でお茶を淹れるだけと思われるかもしれませんが、そこには奥深いものがあります。美味しいお茶を淹れるには、茶葉の量やお湯の量、お湯の温度、抽出時間など全てがうまく噛み合わないといけません。これらを考慮してお客様の好む味を自由自在に出せるようになるには時間がかかります。また、お茶を淹れる時に邪念が入っても美味しいお茶が出せないと稽古を積む中で感じております。忙しい毎日のなかで、心を落ち着けて時間を楽しみ自分と向き合える煎茶道は、自分にとって『心の洗濯機』です。<br>
+          　煎茶自体は日々口にすることが多いものですので、煎茶道を学べばすぐに日々の生活に役立てることができます。また、作法を身に着ける中で所作が綺麗になるかもしれません。<br>
+          　将来、日本茶が広まりつつある世界に、煎茶道という伝統文化も広めていきたいと思っております。<br>
+          　「煎茶道を知りたい」「美味しいお茶の淹れ方を知りたい」という方は是非お気軽にご連絡ください。（コンタクトページのメールアドレスからお願いします）<br>
+        </p>
+      </div>
+    </div>
+    <img src="../../assets/favorites/sencha_3.jpg" class="sencha1">
+    <img src="../../assets/favorites/sencha_1.jpg" class="sencha2">
+    <img src="../../assets/favorites/sencha_2.jpg" class="sencha3">
+    <div class="game">
+      <div class="game--content">
+        <h1>ゲーム</h1>
+        <p>
+          　ゲームは幼稚園の頃から楽しんでいる趣味の一つです。最近はFPS（First Person Shooter：主人公本人視点のゲーム）が好きです。<br>
+          　ゲームは自分にとって活動するための「ガゾリン」みたいなものです。なので、大学受験浪人中も欠かさずやっていました（ただし、勉強8時間しないとできないルールを課していましたが、、）。<br>
+          　ゲームでは、主人公が己の力や仲間との協力で困難を乗り越える姿、刺激的な人生が描写されています。そのようなゲームが小さい頃から身近にあったことから、ゲームは人格形成や人生の指針に影響したと感じております。<br>
+          　人格形成では、小さい頃に独特なゲームをやっていたことが影響してか、周りからはよく「真面目だけど変わってる・独特」と言われます。<br>
+          　人生の指針では「ＲＰＧの主人公」になることを目標に掲げるようになりました。ＲＰＧには、刺激的なフィールドとクエスト、そこに息づく多くの人々、仲間の存在が必要不可欠です。そのＲＰＧの主人公として、自らの行動で社会や多数の人に影響を及ぼせる力を持つ、唯一無二の存在になりたいと考えております。<br>
+          　これからも、ゲームを通しても色々な経験を積んでいきたいと思っています。<br><br>
+          好きなゲーム（抜粋）<br>
+          『クーロンズゲート』：幼稚園の頃の出会い、これを超える世界観のゲームには未だないと感じております。（ページ下の動画で流しているものです）<br>
+          『DOOM』：海外では有名なFPSの原点のゲームです。これも幼稚園の頃から親しんでおりました。<br>
+          『鬼武者』：恐怖に打ち勝つ心を養ってくれました。3部作目の影響でフランス好きになりました。<br>
+        </p>
+      </div>
+    </div>
+    <div class="koolon">
+      <iframe src="https://www.youtube.com/embed/ghHYaPYAXjY?loop=1&autoplay=1&rel=0&playlist=ghHYaPYAXjY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+    <div class="movie">
+      <div class="movie--content">
+        <h1>映画</h1>
+        <p>
+          　映画は幼稚園の頃から楽しんでいる趣味の一つです。特に、アクションが好きで、小さい頃はジャッキー・チェンさんの映画ばかりみていました。<br>
+          　昔はホラーやグロテスクな映画が苦手でしたが、徐々に慣らすことで、最近ではホラー・グロテスクな映画も好きになりました。<br>
+          　映画もゲームと同様、主人公が己の力や仲間との協力で困難を乗り越える姿、刺激的な人生が描写されており、自分に影響を与えたものの一つだと思います。ゲームが自分にとって活動するための「ガソリン」なら、映画は「エンジン」かなと思います。<br><br>
+          好きな映画（抜粋）<br>
+          『スターシップトゥルーパーズ』：SF映画です。小さい頃からなぜか好きで、何十回も見返しています。<br>
+          『プロジェクトイーグル』：ジャッキー・チェンさんの映画です。コメディタッチがあって面白いです。<br>
+          『JM』：キアヌ・リーブス主演のSF映画です。あまり知られていませんが、北野武さんとも共演している映画です。自分はこの映画で液体窒素を初めて知りました。<br><br>
+          グロテスクに慣れる映画<br>
+          『ソウ』：言わずと知れたソリッドシチュエーションホラーです。1作目はサスペンスの部分が主なので慣らしには最適です。<br>
+          『CUBE』：こちらもソリッドシチュエーションホラーです。目覚めるとそこは立方体の連なる部屋。そこからの脱出を描きます。<br><br>
+          ホラーおすすめ映画<br>
+          『リング』：実はガリガリのホラーではなく、謎解き要素が多いサスペンスです。喰わず嫌いしていましたが、この映画でホラーが好きになりました。<br>
+          『フッテージ』：洋画ホラーです。8ミリフィルムに残る惨殺映像。誰がとったものなのか、そこに映る不気味な人物の影は誰なのか。日本的なホラーさを感じる映画です。<br>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .content{
+    margin-top: -350px;
+    height: 120%;
+    background-image: url("../../assets/favorites/back.jpg");
+    background-size: 100% 100%;
+    text-align: center;
+  }
+  .sencha{
+    width: 100%;
+    padding-top: 20px;
+  }
+  .sencha--content{
+    background-image: url("../../assets/favorites/1.png");
+    background-size: 100% 100%;
+    padding: 37% 10%;
+    margin: 5%;
+    font-family: "Sawarabi Mincho";
+  }
+  .sencha1{
+    width: 80%;
+  }
+  .sencha2{
+    margin-right: 5%;
+    margin-top: 20px;
+    width: 40%;
+    display: inline-block;
+  }
+  .sencha3{
+    width: 40%;
+    display: inline-block;
+  }
+  .game{
+    width: 100%;
+    padding-top: 20px;
+  }
+  .game--content{
+    background-image: url("../../assets/favorites/3.png");
+    background-size: 100% 100%;
+    padding: 28% 10%;
+    margin: 5%;
+    font-family: helvetica, arial, 'hiragino kaku gothic pro', meiryo, 'ms pgothic', sans-serif;
+  }
+  .game--content p,
+  .game--content h1{
+    background-color: black;
+    color: white;
+  }
+  .movie{
+    width: 100%;
+    padding-top: 20px;
+  }
+  .movie--content{
+    background-image: url("../../assets/favorites/5.png");
+    background-size: 100% 100%;
+    padding: 37% 10%;
+    margin: 5%;
+    font-family: helvetica, arial, 'hiragino kaku gothic pro', meiryo, 'ms pgothic', sans-serif;
+  }
+  p{
+    text-align: left;
+    z-index: 2;
+    background-color: white;
+    font-size: calc(1.0rem + ((1vw - 0.64rem) * 2.1429));
+  }
+  h1{
+    font-size: calc(3.0rem + ((1vw - 0.64rem) * 2.1429));
+    font-weight: bold;
+  }
+  .koolon{
+    margin: 2% 5%;
+    background-image: url("../../assets/favorites/4.png");
+    background-size: 100% 100%;
+    position:relative;
+    width: 90%;
+    padding-top: 56.25%;
+  }
+  .koolon iframe{
+    position:absolute;
+    top:10.5%;
+    right:7.9%;
+    width:84.5%;
+    height:78.5%;
+  }
+</style>
