24aad3355e80af5dc3bd34357172a9318c47f902
AtoraYamada <atora2018@gmail.com>
2019-08-30 04:11:48 +0900

    adapt delay-loading

--- src/router.js
+++ src/router.js
@@ -1,10 +1,15 @@
 import Vue from "vue";
 import Router from "vue-router";
 
-import Footer from "./components/Top/Footer.vue";
-import Profile from "./components/Profile/Profile.vue";
-import Favorites from "./components/Favorites/Favorites.vue";
-import Contact from "./components/Contact/Contact.vue";
+// import Footer from "./components/Top/Footer.vue";
+// import Profile from "./components/Profile/Profile.vue";
+// import Favorites from "./components/Favorites/Favorites.vue";
+// import Contact from "./components/Contact/Contact.vue";
+
+const Footer = () => import ("./components/Top/Footer.vue"); 
+const Profile = () => import ("./components/Profile/Profile.vue"); 
+const Favorites = () => import ("./components/Favorites/Favorites.vue"); 
+const Contact = () => import ("./components/Contact/Contact.vue"); 
 
 Vue.use(Router);
 
